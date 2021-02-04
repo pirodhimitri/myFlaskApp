1399adc1b3995c39d10da7c785874614d75e43db
pirodhimitri <piro_d@hotmail.com>
2021-02-04 18:44:33 -0500

    push my app

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://u0983jf093:093840934if@something.elephantsql.com/jwirefu094"
+db = SQLAlchemy(app)
+
+
+
+## FRONT_END ROUTES
+@app.route("/")
+def main(): 
+    return render_template("index.html")
+
+
+## SERVICE ROUTES
+@app.route("/api/main/torontocovidcases")
+def firstRoute(): 
+    data = db.session.query(Cases.City, Cases.Count).filter(Cases.city =="Toronto").all()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run()
